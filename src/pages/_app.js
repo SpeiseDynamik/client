fd7219188ef4914f6b374d823e075cc01c119421
Dmitry Kuznetsov <dmitry.kuznetsov.it@gmail.com>
2023-12-25 19:41:58 +0100

    add registartion page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import { AppCacheProvider } from '@mui/material-nextjs/v14-pagesRouter';
-import '@/styles/globals.css';
+// import '@/styles/globals.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
---
+++ src/pages/registration.jsx
@@ -0,0 +1,11 @@
+// @ts-check
+
+function RegistrationPage() {
+  return (
+    <div>
+      <h1>Registration</h1>
+    </div>
+  );
+}
+
+export default RegistrationPage;
