39228873d2f11b4c94b2e55bd35907bd1968da49
Dmitry Kuznetsov <dmitry.kuznetsov.it@gmail.com>
2023-12-29 20:38:41 +0100

    add react-query provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,10 @@
+import { useState } from 'react';
 import { AppCacheProvider } from '@mui/material-nextjs/v14-pagesRouter';
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 // import '@/styles/globals.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -7,9 +13,14 @@ import '@fontsource/roboto/700.css';
 
 export default function App(props) {
   const { Component, pageProps } = props;
+  const [queryClient] = useState(() => new QueryClient());
   return (
-    <AppCacheProvider {...props}>
-      <Component {...pageProps} />
-    </AppCacheProvider>
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={pageProps.dehydratedState}>
+        <AppCacheProvider {...props}>
+          <Component {...pageProps} />
+        </AppCacheProvider>
+      </HydrationBoundary>
+    </QueryClientProvider>
   );
 }
